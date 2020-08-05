58be3ba1f301231e0df0afec3e67a8c669f05387
Ariel <arielbs1991@gmail.com>
2020-08-05 09:37:44 -0700

    added deployed port to server file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -26,3 +26,15 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+// {
+//   "_id": "heroku_7n2jqhsz.budget_db",
+//     "user": "budget_db",
+//       "db": "heroku_7n2jqhsz",
+//         "roles": [
+//           {
+//             "role": "dbOwner",
+//             "db": "heroku_7n2jqhsz"
+//           }
+//         ]
+// };
